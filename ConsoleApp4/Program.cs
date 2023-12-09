f8d64c00fae3e68f37e015f35755ebca19ae9e76
Qurban-93 <qurban231293@gmail.com>
2023-12-09 14:54:32 +0400

    first

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] numbers = { 1, 2, 2, 4, 7, 90, 9, 8, 5 };
+int mostHigh = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > mostHigh)
+    {
+        mostHigh= numbers[i];
+    }
+}
+Console.WriteLine(mostHigh);
+
+
+int number = 45;
+int count = 0;  
+
+for (int i = 1; i <= number; i++)
+{
+    if (number % i == 0)
+    {
+        count++;
+    }
+}
+
+if (count > 2)
+{
+    Console.WriteLine("Murekkeb");
+}
+else
+{
+    Console.WriteLine("Sade");
+}
